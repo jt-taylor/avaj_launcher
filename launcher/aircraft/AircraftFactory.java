191697b1d5f221b9af58d30985fe7861e6a10e4a
Jackson Taylor <jtttaylorjackson@gmail.com>
2019-12-06 18:40:49 -0800

    still need to do figure out how the observer thingy works , then intergrate that into the weather package

--- launcher/aircraft/AircraftFactory.java
+++ launcher/aircraft/AircraftFactory.java
@@ -5,12 +5,21 @@ import java.io.*;
 
 //todo Flyable class and (?) Coordinates
 public class AircraftFactory {
-	//todo 
-	// Flyable
 	public satic Flyable newAircraft(String type, String name, int longitude, int latitude, int altitude) {
 		Coordinates coordinates = new Coordinates (longiture, latitude, altitude);
 		String tmp = type.toLowerCase();
+		//probably a better way to do this
 		if (tmp.equals("baloon"))
-			return new Baloon(name, coordinates)
+			return new Baloon(name, coordinates);
+		else if (tmp.equals("Helicopter"))
+			return new Helicopter(name, coordinates);
+		else if (tmp.equals("JetPlane"))
+			return new JetPlane(name, coordinates);
+		else
+			// maybe not the right exception to throw here ?
+			// not really sure , people seem to be really picky with the runtime exceptions
+			// or maybe just return null i guess
+			throw new IllegalArgumentException("AirCraft Type not Found");
+		return NULL;
 	}
 }
---
+++ launcher/weather/tower.java
@@ -0,0 +1,28 @@
+package launcher.weather;
+
+import launcher.*;
+
+import java.util.*;
+
+public class Tower {
+	//is this how this is supposed to work >?
+	//still not confident that i really understand the whole observer thingy
+	//think that I am over complicating it
+	private Stack<Flyable> observers = new ArrayStack<>();
+
+	//add a unique entrie to the stack
+	public void		register(Flyable fly) {
+		if (!observers.contains(fly))
+			observers.add(fly);
+	}
+	//remove from the stac
+	public void unregister(Flyable fly) {
+		observers.remove(fly);
+	}
+
+	//when updated update all the entries in the stack
+	protected void conditionsChanged() {
+		for (int i = 0;i < observers.size();i++)
+			observers.get(i)updateConditions();
+	}
+}
